5c8ab7b4f25491c7d07384538ecc52aec0645394
FengxiangYang <18380116198@163.com>
2016-12-30 15:15:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
-elif which yum >/dev/null; then
-	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
+elif which dnf >/dev/null; then
+	sudo dnf install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
 fi
 
 ##Add HomeBrew support on  Mac OS
